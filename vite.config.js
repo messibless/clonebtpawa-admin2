6adaa8ddee7e8dece4736b5e85d38568117d5347
ambokileB <wholemike2703email.com>
2026-02-27 09:15:57 +0300

    feat: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/admin2/',  // 🔥 Hii itafanya assets zitafute kwenye /admin2/assets/
   plugins: [
     vue(),
     tailwindcss(),
-
   ],
 })
-
-
-
-
-
-
-
